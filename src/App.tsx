0cd796f5617a9e80ccf1194900477474ee54660c
ahreum.J <jar@ibstech.kr>
2025-08-01 10:26:19 +0900

    깃허브 링크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './components/LanguageSwitcher';
 import ThemeSwitcher from './components/ThemeSwitcher';
+import GitHubLink from './components/GitHubLink';
 import { FileUploadForm } from './components/FileUploadForm';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 	return (
 		<div className="h-screen bg-gradient-to-br from-purple-50 to-blue-50 dark:from-gray-900 dark:to-gray-800 transition-colors overflow-hidden font-display">
 			<div className="absolute top-4 right-4 flex gap-2 z-10">
+				<GitHubLink />
 				<ThemeSwitcher />
 				<LanguageSwitcher />
 			</div>
