eac11c85d3fa12c026a79a8f7423323b38bb2e3d
ahreum.J <jar@ibstech.kr>
2025-12-15 08:45:29 +0900

    로직-ui 분리

--- src/components/FileUploadForm/hook/useProjectNameForm.ts
+++ src/components/FileUploadForm/hook/useProjectNameForm.ts
@@ -1,1 +1,65 @@
-export const useProjectNameForm = () => {};
+import { useFormData } from '@/contexts/FormContext';
+import { SidebarProject } from '@/types/conversation';
+import { useFieldArray, useForm } from 'react-hook-form';
+import { SIDEBAR_KEY } from '../constant';
+import { useEffect } from 'react';
+
+interface resultItem extends SidebarProject {
+	// react-hook-form에서 id를 자체 생성하기 때문에 원본 키값 보존용
+	_id: string;
+}
+
+type ProjectNames = {
+	result: resultItem[];
+};
+
+export const useProjectNameForm = () => {
+	const { sidebarItems, setSidebarItems, setStep } = useFormData();
+	const { control, register, setValue, handleSubmit } = useForm<ProjectNames>();
+	const { fields } = useFieldArray({
+		control,
+		name: 'result',
+	});
+
+	const submit = ({ result }: ProjectNames) => {
+		const _result = result.filter((item) => !!item.title.trim());
+		if (_result.length > 0) {
+			const stored = localStorage.getItem(SIDEBAR_KEY);
+			const prev = stored ? JSON.parse(stored) : {};
+			let update = { ...prev };
+			_result.forEach((item) => {
+				update[item._id] = item.title.trim();
+			});
+
+			localStorage.setItem(SIDEBAR_KEY, JSON.stringify(update));
+			setSidebarItems((prev) =>
+				prev.map((si) => {
+					if ('children' in si) {
+						const title = update[si.id] || '';
+						if (!!title) {
+							return { ...si, title };
+						} else return si;
+					} else return si;
+				})
+			);
+		}
+		setStep('select_target');
+	};
+
+	useEffect(() => {
+		if (sidebarItems.length > 0) {
+			setValue(
+				'result',
+				sidebarItems
+					.filter((item) => 'children' in item)
+					.map((d) => ({ ...d, _id: d.id }))
+			);
+		}
+	}, [sidebarItems]);
+
+	return {
+		submit: handleSubmit(submit),
+		fields,
+		register,
+	};
+};
--- src/components/FileUploadForm/ui/ProjectNameForm.tsx
+++ src/components/FileUploadForm/ui/ProjectNameForm.tsx
@@ -1,12 +1,10 @@
 import { SubmitButton } from '@/components/Buttons';
 import { TextInput } from '@/components/Form';
-import { useFormData } from '@/contexts/FormContext';
 import { SidebarProject } from '@/types/conversation';
 import { PropsWithChildren, useEffect, useState } from 'react';
-import { useFieldArray, useForm } from 'react-hook-form';
 import { LuFolder, LuFolderOpen } from 'react-icons/lu';
-import { SIDEBAR_KEY } from '../constant';
 import { BiChevronsDown } from 'react-icons/bi';
+import { useProjectNameForm } from '../hook/useProjectNameForm';
 
 type ChildrenListProps = PropsWithChildren<{
 	list: Pick<SidebarProject, 'children'>['children'];
@@ -60,62 +58,13 @@ const ChildrenList = ({ children, list }: ChildrenListProps) => {
 	);
 };
 
-interface resultItem extends SidebarProject {
-	// react-hook-form에서 id를 자체 생성하기 때문에 원본 키값 보존용
-	_id: string;
-}
-
-type ProjectNames = {
-	result: resultItem[];
-};
-
 export const ProjectNameForm = () => {
-	const { sidebarItems, setSidebarItems } = useFormData();
-	const { control, register, setValue, handleSubmit } = useForm<ProjectNames>();
-	const { fields } = useFieldArray({
-		control,
-		name: 'result',
-	});
-
-	const submit = ({ result }: ProjectNames) => {
-		const _result = result.filter((item) => !!item.title.trim());
-		if (_result.length > 0) {
-			const stored = localStorage.getItem(SIDEBAR_KEY);
-			const prev = stored ? JSON.parse(stored) : {};
-			let update = { ...prev };
-			_result.forEach((item) => {
-				update[item._id] = item.title.trim();
-			});
-
-			localStorage.setItem(SIDEBAR_KEY, JSON.stringify(update));
-			setSidebarItems((prev) =>
-				prev.map((si) => {
-					if ('children' in si) {
-						const title = update[si.id] || '';
-						if (!!title) {
-							return { ...si, title };
-						} else return si;
-					} else return si;
-				})
-			);
-		}
-	};
-
-	useEffect(() => {
-		if (sidebarItems.length > 0) {
-			setValue(
-				'result',
-				sidebarItems
-					.filter((item) => 'children' in item)
-					.map((d) => ({ ...d, _id: d.id }))
-			);
-		}
-	}, [sidebarItems]);
+	const { submit, fields, register } = useProjectNameForm();
 
 	return (
 		<form
 			className="flex flex-col overflow-hidden size-full gap-4"
-			onSubmit={handleSubmit(submit)}
+			onSubmit={submit}
 		>
 			<h2 className="text-gray-800 dark:text-gray-100">프로젝트 이름 편집</h2>
 			<ul className="flex-1 overflow-auto">
